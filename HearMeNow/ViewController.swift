c426c16202d3d278ac0f6b5a92c802899b2954a8
Huangguozheng <huangguozheng@licaigc.com>
2017-09-24 14:59:55 +0800

    Initial Commit

---
+++ HearMeNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HearMeNow
+//
+//  Created by Hamilton Huang on 24/09/2017.
+//  Copyright © 2017 Cirray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
